158157c1211cbbec38a3a4194d1a6634e8ce629b
Philippe Brossier <brossierp@gmail.com>
2020-02-01 21:15:33 +0000

    1st push. Testing controller OK

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# springBootAndKubernetes
+# To build
+./gradlew clean build
+
+# To run
+./gradlew bootRun
+
+# To test
+curl http://localhost:8080/greeting/ -v -X GET
+200 {"id":1,"content":"Hello, World!"}
---
+++ src/main/java/com/example/kube/SpringBootAndKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.kube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndKubernetesApplication.class, args);
+	}
+
+}
